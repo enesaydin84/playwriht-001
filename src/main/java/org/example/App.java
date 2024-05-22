23ba7b632267aef2edc5c5946d96398be0245630
enesaydin84 <enesaydin4520@gmail.com>
2024-05-22 15:38:08 +0300

    Set Headless false

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
@@ -8,10 +9,8 @@ public class App
 {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Page page;
-            try (Browser browser = playwright.chromium().launch()) {
-                page = browser.newPage();
-            }
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
             page.navigate("http://playwright.dev");
             System.out.println(page.title());
         }
