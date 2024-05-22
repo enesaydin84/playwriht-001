00098b0f9b43d7ce7df8f474c572dce659934933
enesaydin84 <enesaydin4520@gmail.com>
2024-05-22 15:13:05 +0300

    pom xml updated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
+    }}
