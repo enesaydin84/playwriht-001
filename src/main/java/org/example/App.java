db2e97ed037b6e4ce3ebd2ba207068e12614bbc6
enesaydin84 <enesaydin4520@gmail.com>
2024-05-23 15:28:06 +0300

    AppTest added...

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,32 +1,11 @@
 package org.example;
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.AriaRole;
-import org.junit.jupiter.api.Test;
 
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
 public class App 
 {
-    @Test
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-            Page page = browser.newPage();
-            page.navigate("https://stb.turkcellbulut.com/");
-            page.getByRole(AriaRole.LINK,new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
-            page.getByPlaceholder("Müşteri numaranızı ya da e-posta adresinizi girin").fill("23350412");
-            page.getByPlaceholder("Şifrenizi Girin").fill("dssdds");
-            page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
-            assertThat(page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Devamkkk"))).isVisible();
-            try {
-                Thread.sleep(15000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+
 
         }
-    }}
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,56 @@
 package org.example;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
-{
+    extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://stb.turkcellbulut.com/");
+            page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
+            page.getByPlaceholder("Müşteri numaranızı ya da e-posta adresinizi girin").fill("23350412");
+            page.getByPlaceholder("Şifrenizi Girin").fill("dssdds");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
+            assertThat(page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Devamkkk"))).isVisible();
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
