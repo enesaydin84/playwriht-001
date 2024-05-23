825eeb299b62f8c069ef904dbb7cd7e2c63aad88
enesaydin84 <enesaydin4520@gmail.com>
2024-05-23 14:57:35 +0300

    TB Login

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 
 
 public class App 
@@ -11,7 +12,16 @@ public class App
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
+            page.navigate("https://stb.turkcellbulut.com/");
+            page.getByRole(AriaRole.LINK,new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
+            page.getByPlaceholder("Müşteri numaranızı ya da e-posta adresinizi girin").fill("23350412");
+            page.getByPlaceholder("Şifrenizi Girin").fill("dssdds");
+            page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Giriş Yapın")).click();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println(page.title());
         }
     }}
